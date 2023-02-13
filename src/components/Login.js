cca1ed1eb430de476cab3987100541115a08cf30
mikiasano28 <shiro.kume.30s2828@gmail.com>
2023-02-13 18:49:45 +0900

    added Input.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useReducer } from 'react';
 
 import Card from './Card';
+import Input from './input';
 import classes from './Login.module.css';
 import Button from './Button';
 
@@ -110,34 +111,24 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input 
+          id="email" 
+          label="E-Mail" 
+          type="email" 
+          isValid={emailIsValid} 
+          value={emailState.value}
+          onChange={emailChangeHandler} 
+          onBlur={validateEmailHandler}
+        />
+        <Input 
+          id="password"
+          label="Password"
+          type="password"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
